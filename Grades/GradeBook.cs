f7526af2c3b918d593adf533d5a9a6ffff21378c
Truc Chau <tc9538@cs.ship.edu>
2018-10-25 17:08:49 -0400

    updated code

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,12 +8,31 @@ namespace Grades
 {
     class GradeBook
     {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            stats.HighestGrade = 0;
+            stats.LowestGrade = 0;
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                sum = sum + grade;
+            }
+            stats.AverageGrade = sum/grades.Count;
+
+            return stats;
+        }
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        List<float> grades;
+        private List<float> grades = new List<float>();
     }
 
     
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -10,8 +10,16 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            GradeBook book;
+            GradeBook book = new GradeBook();
             book.AddGrade(91);
+            book.AddGrade(89.5f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+           
+
+            book = new GradeBook();
+            book.AddGrade(75);
+
         }
     }
 
