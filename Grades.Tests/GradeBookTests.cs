64b0785a04d5b35b9932df305551cffa49697b10
Truc Chau <tc9538@cs.ship.edu>
2019-03-03 12:49:57 -0500

    changes

--- Grades.Tests/GradeBookTests.cs
+++ Grades.Tests/GradeBookTests.cs
@@ -8,8 +8,10 @@ using System.Threading.Tasks;
 
 namespace Grades.Tests
 {
-    class GradeBookTests
+    [TestClass]
+    public class GradeBookTests
     {
+        [TestMethod]
         public void ComputeHighestGrade()
         {
             GradeBook gb = new GradeBook();
@@ -18,8 +20,18 @@ namespace Grades.Tests
 
             GradeStatistics result = gb.ComputeStatistics();
             Assert.AreEqual(90, result.HighestGrade);
-
         }
-        
+
+        [TestMethod]
+        public void ComputeLowestGrade()
+        {
+            GradeBook gb = new GradeBook();
+            gb.AddGrade(10);
+            gb.AddGrade(90);
+
+            GradeStatistics result = gb.ComputeStatistics();
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+
     }
 }
